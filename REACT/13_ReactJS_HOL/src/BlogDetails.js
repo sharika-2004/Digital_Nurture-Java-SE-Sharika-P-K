818ba36aee94c9ac85bd9bbae39df22fe8e82918
Sharika P K <sharikavipanchika@gmail.com>
2026-07-07 15:21:41 +0530

    Add files via upload

---
+++ REACT/13_ReactJS_HOL/src/BlogDetails.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function BlogDetails() {
+
+  const content = (
+    <div>
+
+      <h2>React Learning</h2>
+
+      <h4>Stephen Biz</h4>
+
+      <p>Welcome to learning React!</p>
+
+      <h2>Installation</h2>
+
+      <h4>Schwzdenier</h4>
+
+      <p>You can install React from npm.</p>
+
+    </div>
+  );
+
+  return content;
+}
+
+export default BlogDetails;
---
+++ REACT/13_ReactJS_HOL/src/CourseDetails.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function CourseDetails() {
+
+  const coursedet = (
+    <div>
+
+      <h2>Angular</h2>
+
+      <h4>4/5/2021</h4>
+
+      <h2>React</h2>
+
+      <h4>6/3/2021</h4>
+
+    </div>
+  );
+
+  return coursedet;
+}
+
+export default CourseDetails;
