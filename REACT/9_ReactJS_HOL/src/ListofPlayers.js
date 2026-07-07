b7036c378fd50682ce2ca7be910ab2dc2448201d
Sharika P K <sharikavipanchika@gmail.com>
2026-07-07 14:32:45 +0530

    Add files via upload

---
+++ REACT/9_ReactJS_HOL/src/ListofPlayers.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const players = [
+  { name: "Jack", score: 50 },
+  { name: "Michael", score: 70 },
+  { name: "John", score: 40 },
+  { name: "Ann", score: 61 },
+  { name: "Elisabeth", score: 61 },
+  { name: "Sachin", score: 95 },
+  { name: "Dhoni", score: 100 },
+  { name: "Virat", score: 84 },
+  { name: "Jadeja", score: 64 },
+  { name: "Raina", score: 75 },
+  { name: "Rohit", score: 80 }
+];
+
+export function ListofPlayers() {
+  return (
+    <ul>
+      {players.map((item, index) => (
+        <li key={index}>
+          Mr. {item.name} {item.score}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export function Scorebelow70() {
+  const players70 = players.filter((item) => item.score <= 70);
+
+  return (
+    <ul>
+      {players70.map((item, index) => (
+        <li key={index}>
+          Mr. {item.name} {item.score}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export { players };
