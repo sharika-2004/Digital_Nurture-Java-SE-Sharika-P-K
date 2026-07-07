4ecfd4ab731381bed5f3a7a0bca3d86e26d94277
Sharika P K <sharikavipanchika@gmail.com>
2026-07-07 15:11:17 +0530

    Create App.js

---
+++ REACT/12_ReactJS_HOL/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import LoginControl from "./LoginControl";
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <LoginControl />
+
+    </div>
+
+  );
+
+}
+
+export default App;
