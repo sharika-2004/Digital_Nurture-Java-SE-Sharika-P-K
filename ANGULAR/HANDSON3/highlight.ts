a7586f728db896e6801dc17974852328b453a9c4
Sharika P K <sharikavipanchika@gmail.com>
2026-07-11 22:16:50 +0530

    Create highlight.ts

---
+++ ANGULAR/HANDSON3/highlight.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core'; 
+import { CourseCardComponent } from '../course-card/course-card';
+import { Course } from '../types/course.interface';
+
+@Component({
+  selector: 'app-course-list',
+  standalone: true,
+  imports: [CommonModule, CourseCardComponent],
+  templateUrl: './course-list.html'
+})
+export class CourseListComponent implements OnInit {
+  isLoading: boolean = true;
+  courses: Course[] = [];
+
+  
+  constructor(private cdr: ChangeDetectorRef) {} 
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.courses = [
+        { id: 101, title: 'Introduction to Angular', credits: 4, enrolled: true, gradeStatus: 'passed' },
+        { id: 102, title: 'Advanced TypeScript & Basics', credits: 3, enrolled: false, gradeStatus: 'pending' },
+        { id: 103, title: 'UI/UX Design Fundamentals', credits: null, enrolled: false, gradeStatus: 'failed' }
+      ];
+      this.isLoading = false;
+      
+      
+      this.cdr.detectChanges(); 
+      console.log("Timer finished! UI updated.");
+      
+    }, 1500); 
+  }
+
+  trackByCourseId(index: number, course: Course): number {
+    return course.id;
+  }
+}
