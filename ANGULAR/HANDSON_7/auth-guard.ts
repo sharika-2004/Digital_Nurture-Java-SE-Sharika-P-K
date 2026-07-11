b26beab4236092c27e74af1901d9a3d96e573995
Sharika P K <sharikavipanchika@gmail.com>
2026-07-12 01:06:15 +0530

    Create auth-guard.ts

---
+++ ANGULAR/HANDSON_7/auth-guard.ts
@@ -0,0 +1,16 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn) {
+    return true;
+  } else {
+    alert('Access Denied: Please toggle the Mock Login status on the Home page first!');
+    router.navigate(['/']);
+    return false;
+  }
+};
