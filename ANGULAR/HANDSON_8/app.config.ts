1338f6396b770975d0b4616516756ee6a32449a8
Sharika P K <sharikavipanchika@gmail.com>
2026-07-12 18:43:07 +0530

    Create app.config.ts

---
+++ ANGULAR/HANDSON_8/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core'; 
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { routes } from './app.routes';
+import { authInterceptor } from './interceptors/auth.interceptor';
+import { errorHandlerInterceptor } from './interceptors/error-handler.interceptor';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZonelessChangeDetection(), 
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor, errorHandlerInterceptor])
+    )
+  ]
+};
