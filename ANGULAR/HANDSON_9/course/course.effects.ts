f25e65f0661a89195196db6e397301e7c4d0f05d
Sharika P K <sharikavipanchika@gmail.com>
2026-07-12 19:39:55 +0530

    Create course.effects.ts

---
+++ ANGULAR/HANDSON_9/course/course.effects.ts
@@ -0,0 +1,26 @@
+import { inject, Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { CourseService } from '../../services/course'; 
+import * as CourseActions from './course.actions';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+
+@Injectable()
+export class CourseEffects {
+  private actions$ = inject(Actions);
+  private courseService = inject(CourseService);
+
+  loadCourses$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(CourseActions.loadCourses),
+      switchMap(() =>
+        this.courseService.getCourses().pipe(
+          map((courses) => CourseActions.loadCoursesSuccess({ courses })),
+          catchError((error) =>
+            of(CourseActions.loadCoursesFailure({ error: error.message || 'Unknown Error' }))
+          )
+        )
+      )
+    )
+  );
+}
