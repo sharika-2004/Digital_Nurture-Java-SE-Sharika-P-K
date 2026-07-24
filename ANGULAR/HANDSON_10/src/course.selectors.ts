9f6ffaba8a84cc7bc3dff59139a51e44f9e82612
Sharika P K <sharikavipanchika@gmail.com>
2026-07-24 19:57:53 +0530

    Create course.selectors.ts

---
+++ ANGULAR/HANDSON_10/src/course.selectors.ts
@@ -0,0 +1,23 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { CourseState } from './course.reducer';
+
+export const selectCourseState =
+createFeatureSelector<CourseState>('course');
+
+export const selectCourses =
+createSelector(
+selectCourseState,
+state => state.courses
+);
+
+export const selectLoading =
+createSelector(
+selectCourseState,
+state => state.loading
+);
+
+export const selectError =
+createSelector(
+selectCourseState,
+state => state.error
+);
